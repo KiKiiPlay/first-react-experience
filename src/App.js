f79d98897ca946c70d533778925cfa5a35c95d54
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-27 13:21:27 +0100

    Completing my first react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent button_name="New Button" />
+      <ImageComponent
+        img="https://picsum.photos/200/300"
+        alt="Random picture"
+      />
     </div>
   );
 }
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,7 @@
+const ButtonComponent = ({ button_name }) => (
+  <>
+    <button>{button_name}</button>
+  </>
+);
+
+export default ButtonComponent;
